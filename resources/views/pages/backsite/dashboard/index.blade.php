e7b569b40061f64098e0efd184add694bd25a7fd
Alinurrr <alinoer.rachman@gmail.com>
2022-09-15 13:49:50 +0700

    update dasboard view for layout, header, menu and footer

--- resources/views/pages/backsite/dashboard/index.blade.php
+++ resources/views/pages/backsite/dashboard/index.blade.php
@@ -1,1 +1,9 @@
-ini halaman dashboard
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+
+@section('content')
+    <!-- Content -->
+    halo ini halaman dashboard
+    <!--End Content -->
+@endsection
