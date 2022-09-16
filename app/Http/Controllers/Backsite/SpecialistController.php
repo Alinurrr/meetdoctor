a7eb2092527be94de7e5b4ce9df011d235a0d251
Alinurrr <alinoer.rachman@gmail.com>
2022-09-16 15:27:24 +0700

    update controller specialist

--- app/Http/Controllers/Backsite/SpecialistController.php
+++ app/Http/Controllers/Backsite/SpecialistController.php
@@ -62,8 +62,12 @@ class SpecialistController extends Controller
     public function store(StoreSpecialistRequest $request)
     {
         // get all request form frontsite
-
         $data = $request->all();
+
+        // store to database
+        $specialist = Specialist::create($data);
+        alert()->success('Success Message', 'Successfully added new specialist');
+        return redirect()->route('backsite.specialist.index');
     }
 
     /**
@@ -72,9 +76,9 @@ class SpecialistController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Specialist $specialist)
     {
-        return abort(404);
+        return view('pages.backsite.master-data.specialist.show', compact('specialist'));
     }
 
     /**
@@ -83,9 +87,9 @@ class SpecialistController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Specialist $specialist)
     {
-        return abort(404);
+        return view('pages.backsite.master-data.specialist.edit', compact('specialist'));
     }
 
     /**
@@ -95,9 +99,16 @@ class SpecialistController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UpdateSpecialistRequest $request, Specialist $specialist)
     {
-        return abort(404);
+        // get all request from frontsite
+        $data = $request->all();
+
+        // update to database
+        $specialist->update($data);
+
+        alert()->success('Success Message', 'Successfully updated specialist');
+        return redirect()->route('backsite.specialist.index');
     }
 
     /**
@@ -106,8 +117,11 @@ class SpecialistController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Specialist $specialist)
     {
-        return abort(404);
+        $specialist->delete();
+
+        alert()->success('Success Message', 'Successfully deleted specialist');
+        return back();
     }
 }
